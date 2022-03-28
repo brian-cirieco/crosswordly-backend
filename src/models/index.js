6c9417779314f0f443183d27aa40686155b6dbd2
Brian-15 <bcirieco@gmail.com>
2022-03-28 16:24:02 -0700

    set production sequelize ssl options: rejectUnauthorized to false - to fix heroku rejection of self-signed certificate

--- src/models/index.js
+++ src/models/index.js
@@ -10,7 +10,16 @@ const db = {};
 
 let sequelize;
 if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+  sequelize = new Sequelize({
+    database: process.env.DATABASE_URL,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
 } else {
   if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
