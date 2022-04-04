1315b6552279a13ea2b090539812d7cf7971015f
Brian-15 <bcirieco@gmail.com>
2022-04-04 15:50:05 -0700

    implemented word generator helper function for trie in JSON format

---
+++ src/helpers/getWordsFromJSON.js
@@ -0,0 +1,23 @@
+function getWordsFromJSON(trieJSON, letters, maxDepth=letters.length, word="") {
+  const words = [];
+  const visited = {};
+  if (trieJSON.isWord) words.push(word);
+  if (!letters.length || word.length >= maxDepth) return words;
+  for (let i = 0; i < letters.length; i++) {
+    const char = letters[i];
+    if (!visited[char]) visited[char] = true;
+    else continue;
+    if (trieJSON[char]) {
+      words.push(...getWordsFromJSON(
+        trieJSON[char],
+        letters.slice(0, i) + letters.slice(i + 1),
+        maxDepth,
+        word + char
+      ));
+    };
+  }
+
+  return words;
+}
+
+module.exports = getWordsFromJSON;
---
+++ src/test/unit/getWordsFromJSON.test.js
@@ -0,0 +1,49 @@
+const getWordsFromJSON = require("../../helpers/getWordsFromJSON");
+
+describe("getWordsFromJSON helper function unit tests", () => {
+  const trieJSON = {
+    'h': {
+      'e': {
+        'l': {
+          isWord: true,
+          'l': {
+            isWord: true,
+            'o': { isWord: true }
+          }
+        }
+      },
+      'i': {
+        isWord: true,
+        's': { isWord: true }
+      },
+    },
+    'l': {
+      'e': {
+        'o': { isWord: true }
+      },
+      'o': {
+        'l': { isWord: true }
+      }
+    },
+  };
+
+  test("finds all words that can be formed with 'hello'", () => {
+    const words = getWordsFromJSON(trieJSON, "hello");
+    expect(words).toEqual([ "hel", "hell", "hello", "leo", "lol" ]);
+  });
+
+  test("will only return 'hi', omitting other words", () => {
+    const words = getWordsFromJSON(trieJSON, "hi");
+    expect(words).toEqual(["hi"]);
+  });
+
+  test("will return empty array if no words match letters provided", () => {
+    const words = getWordsFromJSON(trieJSON, "blabla");
+    expect(words.length).toBe(0);
+  });
+
+  test("will return empty array if no letters are provided", () => {
+    expect(getWordsFromJSON(trieJSON, "")).toEqual([]);
+  });
+
+});
