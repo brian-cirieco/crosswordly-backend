ba3492be0ed57c00d450dcd7ac06421261b176f2
Brian-15 <bcirieco@gmail.com>
2022-04-05 15:48:44 -0700

    forced sync complete & changed table name to dictionaries

--- src/migrations/20220401212645-create-dictionary.js
+++ src/migrations/20220401212645-create-dictionary.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Dictionaries', {
+    await queryInterface.createTable('dictionaries', {
       id: {
         allowNull: false,
         autoIncrement: true,
--- src/seeders/20220401212306-dictionary.js
+++ src/seeders/20220401212306-dictionary.js
@@ -31,6 +31,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-    await queryInterface.bulkDelete('dictionary', null, {});
+    await queryInterface.bulkDelete('dictionaries', null, {});
   }
 };
