61e4b74da1a01480298107edf7a0449f998e23ad
Brian-15 <bcirieco@gmail.com>
2022-04-11 14:42:54 -0700

    update migration

--- src/migrations/20220321205958-create-user.js
+++ src/migrations/20220321205958-create-user.js
@@ -6,10 +6,7 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.UUID
       },
       username: {
         type: Sequelize.STRING
